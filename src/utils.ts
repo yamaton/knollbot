a4972ddedd1f39dda21891c5b801a20ba5c8b2f0
Yamato Matsuoka <yamato.matsuoka@tetradresearch.com>
2020-08-28 23:39:37 -0400

    Add randn function

--- src/utils.ts
+++ src/utils.ts
@@ -124,6 +124,18 @@ namespace utils {
         return a + Math.floor(Math.random() * (b - a) / unit) * unit
     }
 
+    // https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+    export const randn = (): number => {
+        let u = 0;
+        let v = 0;
+        while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+        while (v === 0) v = Math.random();
+        let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+        num = num / 10.0 + 0.5; // Translate to 0 -> 1
+        if (num > 1 || num < 0) return randn(); // resample between 0 and 1
+        return num - 0.5;
+    }
+
     export const unitVec = (from: Vector, to: Vector): Vector => {
         let d = distEuclid(from, to)
         return {

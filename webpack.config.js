24386a50aa753e442d7f18ba019c334e2f59fb52
Yamato Matsuoka <yamato.matsuoka@tetradresearch.com>
2020-09-03 22:47:53 -0400

    Adapt to webpack ts-loader
    Now one can build directly from TS by `npm run build`.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
+const path = require('path');
+
 module.exports = {
-  entry: './built/src/index.js'
+  mode: 'development',  // 'development' or 'production'
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'built'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
